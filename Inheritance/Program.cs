f2b83f76e148a341d4c16422082fba0784704ce2
csherwood98 <zsherwood98@gmail.com>
2021-02-03 11:19:18 -0600

    task finished

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -4,36 +4,65 @@ namespace Inheritance
 {
     class Program
     {
+        public class Animal
+        {
+            public int Age { get; set; }
+            public int Height { get; set; }
+            public int Width { get; set; }
+            public bool BiggerThanBreadbox { get; set; }
+
+        }
+
+        public class Bird : Animal
+        {
+            public int Wingspan { get; set; }
+            public int BeakLength { get; set; }
+            public string SpeciesName { get; set; }
+            public bool CanFly { get; set; }
+        }
+        public class Reptile : Animal
+        {
+            public int TailLength { get; set; }
+            public int TeethCount { get; set; }
+            public string Color { get; set; }
+            public bool IsSnake { get; set; }
+        }
         static void Main(string[] args)
         {
-            // TODO Be sure to follow best practice when creating your classes
-
-            // Create a class Animal
-            // give this class 4 members that all Animals have in common
-
-
-            // Create a class Bird
-            // give this class 4 members that are specific to Bird
-            // Set this class to inherit from your Animal Class
-
-            // Create a class Reptile
-            // give this class 4 members that are specific to Reptile
-            // Set this class to inherit from your Animal Class
-
-
-
-
             /*Create an object of your Bird class
              *  give values to your members using the object of your Bird class
              *  
              * Creatively display the class member values 
              */
+            Bird crossbillFinch = new Bird()
+            {
+                Age = 1,
+                Height = 2,
+                Width = 1,
+                BiggerThanBreadbox = false,
+                Wingspan = 1,
+                BeakLength = 1,
+                SpeciesName = "Crossbill",
+                CanFly = true
+            };
 
+            Reptile diamondbackSnake = new Reptile()
+            {
+                Age = 3,
+                Height = 1,
+                Width = 1,
+                BiggerThanBreadbox = true,
+                TailLength = 20,
+                TeethCount = 2,
+                Color = "Brown, Tan, Yellow",
+                IsSnake = true,
+            };
             /*Create an object of your Reptile class
              *  give values to your members using the object of your Bird class
              *  
              * Creatively display the class member values 
              */
+            Console.WriteLine($"This {crossbillFinch.SpeciesName} is {crossbillFinch.Age} years old. It has dimensions of {crossbillFinch.Height} inches tall and {crossbillFinch.Width} inches wide with an impressive wingspan of {crossbillFinch.Wingspan} inches and a mighty beak {crossbillFinch.BeakLength} centimeters long. If told that this gracious creature is larger than a breadbox, the statement is {crossbillFinch.BiggerThanBreadbox}");
         }
     }
 }
